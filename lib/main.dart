8dc34a9a8d9c973b6f4516658c35c67b7d6f7304
Matheus <jmatheuscosta17@gmail.com>
2023-12-28 03:21:07 -0300

    tela home ( que vai virar sobre nos provavelmente )

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,128 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ifaltou?',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(77,167,104, 1)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
+
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: const Text('Ifaltou?', style: TextStyle(color: Color.fromRGBO(77,167,104, 1), fontSize: 45, fontWeight: FontWeight.bold,)),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.home),
+            tooltip: 'Home',
+            iconSize: 40,
+            onPressed: () {},
+          ),
+          const SizedBox(width: 30),
+          IconButton(
+            icon: Text("Salas Abertas", style: TextStyle(color: const Color.fromARGB(255, 0, 0,0), fontSize: 25)),
+            //icon: const Icon(Icons.door_back_door_outlined ),
+            tooltip: 'Salas Abertas',
+            iconSize: 40,
+            onPressed: () {},
+          ),
+          const SizedBox(width: 30),
+          IconButton(
+            icon: Text("Perfil", style: TextStyle(color: const Color.fromARGB(255, 0, 0,0), fontSize: 25)),
+            //icon: const Icon(Icons.account_circle_outlined),
+            tooltip: 'Perfil',
+            iconSize: 40,
+            onPressed: () {},
+          ),
+          const SizedBox(width: 30),
+          Padding(
+            padding: const EdgeInsets.only(right: 50), // ajuste o valor de acordo com suas necessidades
+            child: Ink(
+              decoration: ShapeDecoration(
+                color: const Color.fromRGBO(77,167,104, 1),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: FittedBox(
+                child: IconButton(
+                  tooltip: 'Login',
+                  //icon: const Icon(Icons.login),                
+                  color: const Color.fromARGB(255,255,255,255),
+                  icon: Text("Login", style: TextStyle(color: const Color.fromARGB(255,255,255,255), fontSize: 25)),
+                  onPressed: () {},
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          )
+        ],
+
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 100), // Espaço adicionado à esquerda
+                    child: Text(
+                      'IFaltou?',
+                      style: TextStyle(fontSize: 128),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 700, // Largura da imagem
+                  height: 600,
+                  padding: const EdgeInsets.only(right: 100),
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30), // Espaço adicionado
+            const Padding(
+              padding: EdgeInsets.only(left: 30), // Espaço adicionado à esquerda
+              child: Text(
+                'O IFaltou? é um sistema projetado para simplificar o controle de presença e frequência de alunos em instituições de ensino. Seu objetivo principal é tornar o acompanhamento da presença mais eficiente e acessível tanto para alunos quanto para professores.',
+                style: TextStyle(fontSize: 40),
+              ),
+            ),
+            const SizedBox(height: 30), // Espaço adicionado
+            const Padding(
+              padding: EdgeInsets.only(left: 30), // Espaço adicionado à esquerda
+              child: Text(
+                'Com o IFaltou?, alunos e professores podem registrar e monitorar facilmente a presença em suas aulas, tornando simples para os estudantes acompanharem sua própria frequência e para os professores gerenciarem a presença em suas turmas.',
+                style: TextStyle(fontSize: 40),
+              ),
+            ),
+            const SizedBox(height: 30), // Espaço adicionado
+            const Padding(
+              padding: EdgeInsets.only(left: 30, bottom: 30), // Espaço adicionado à esquerda
+              child: Text(
+                'Este sistema oferece uma maneira conveniente e eficaz de manter o controle de presença, resultando em um ambiente educacional mais organizado. Os alunos podem acompanhar seu desempenho de forma transparente, enquanto os professores podem manter registros precisos.',
+                style: TextStyle(fontSize: 40),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )  
     );
   }
 }
